51f994630f12c1a115d2123a5fe987556c14f365
Ryan Julian <ryanjulian@berkeley.edu>
2012-08-30 10:19:17 -0700

    Initial add of new Python particle filter

---
+++ particle_filter/particle_filter.py
@@ -0,0 +1,71 @@
+from numpy import *
+from numpy.random import *
+
+# SciPy Particle Filter Example
+# http://www.scipy.org/Cookbook/ParticleFilter
+def resample(weights):
+    n = len(weights)
+    indices = []
+    C = [0.] + [sum(weights[:i+1]) for i in range(n)]
+    u0, j = random(), 0
+    for u in [(u0+i)/n for i in range(n)]:
+        while u > C[j]:
+            j+=1
+        indices.append(j-1)
+    return indices
+
+
+def particlefilter(sequence, pos, stepsize, color_model, n):
+    seq = iter(sequence)
+    x = ones((n, 2), int) * pos                     # Initial position
+    f0 = color_model * ones(n)                      # Target colour model
+    yield pos, x, ones(n)/n                         # Return expected position, particles and weights
+    for im in seq:
+        x += uniform(-stepsize, stepsize, x.shape)  # Particle motion model: uniform step
+        x  = x.clip(zeros(2), array(im.shape)-1).astype(int) # Clip out-of-bounds particles
+        f  = im[tuple(x.T)]                         # Measure particle colours
+        w  = 1./(1. + (f0-f)**2)                    # Weight~ inverse quadratic colour distance
+        w /= sum(w)                                 # Normalize w
+        yield sum(x.T*w, axis=1), x, w              # Return expected position, particles and weights
+        if 1./sum(w**2) < n/2.:                     # If particle cloud degenerate:
+            x  = x[resample(w),:]                   # Resample particles according to weightsfrom numpy import *
+################################################################################
+
+# Based loosely on the BallBot particle filter
+# https://github.com/radicalnerd/ballbot/
+class ParticleFilter:
+    # TODO: Generic transition and emission models
+
+    def __init__(self, pos, stepsize, color_model, num_particles=1000):
+        self.num_particles = num_particles
+        self.particles = ones((n,2), int) * pos     # Initial position
+        self.f0 = color_model * ones(num_particles)
+        self.pos = pos
+        self.w = ones(num_particles)/num_particles
+        
+    def observe(self, observation):
+        f = observation[tuple(self.particles.T)]    # Measure particle colors
+        self.w = 1./(1. + (self.f0-f)**2)           # Weight ~ inverse quadratic color distance
+        self.w /= sum(w)                            # Normalize w
+        if 1./sum(w**2) < n/2.:                     # Resample if particles collapse
+            self.particles = self.particles[resample(w),:]
+    
+    def elapse_time(self):
+        self.particles += uniform(-stepsize, stepsize, x.shape)                         # Uniform step motion model
+        self.particles = self.particles.clip(zeros(2), array(im.shape)-1.astype(int))   # Clip out-of-bounds particles
+        
+    def get_beliefs(self):
+        return self.particles
+        
+    def get_estimate(self):
+        return sum(self.particles.T*self.w, axis=1)
+        
+    def get_weights(self):
+        return self.w
+        
+    def get_deprecated(self):
+        return self.get_estimate(), self.get_beliefs(), self.get_weights()
+    
+    
+    
+    
